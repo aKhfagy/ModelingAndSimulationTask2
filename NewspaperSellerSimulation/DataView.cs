91042021d86ada772c8bbdee008420ef2d4c667d
aKhfagy <52210948+aKhfagy@users.noreply.github.com>
2022-04-10 01:10:07 +0200

    Finished GUI

--- NewspaperSellerSimulation/DataView.cs
+++ NewspaperSellerSimulation/DataView.cs
@@ -17,72 +17,51 @@ namespace NewspaperSellerSimulation
         {
             InitializeComponent();
             DataTable dt = new DataTable();
-            //dt.Columns.Add("Customer No.", typeof(int));
-            //dt.Columns.Add("Random Digit For Arrivals", typeof(int));
-            //dt.Columns.Add("Time Between Arrivals", typeof(int));
-            //dt.Columns.Add("Clock Time of Arrivals", typeof(int));
-            //dt.Columns.Add("Random Digit For Service", typeof(int));
-            //for (int i = 0; i < system.NumberOfServers; i++)
-            //{
-            //    dt.Columns.Add("Server " + (i + 1) + ": Time Service Begins", typeof(int));
-            //    dt.Columns.Add("Server " + (i + 1) + ": Service Time", typeof(int));
-            //    dt.Columns.Add("Server " + (i + 1) + ": Time Service Ends", typeof(int));
-            //}
-            //dt.Columns.Add("Time in Queue", typeof(int));
+            dt.Columns.Add("Day No.", typeof(int)); // 0
+            dt.Columns.Add("Random Digit For Type", typeof(int)); // 1
+            dt.Columns.Add("Type", typeof(string)); // 2
+            dt.Columns.Add("Random Digit for Demand", typeof(int)); // 3
+            dt.Columns.Add("Demand", typeof(int)); // 4
+            dt.Columns.Add("Sales Revenue", typeof(decimal)); // 5
+            dt.Columns.Add("Loss", typeof(decimal)); // 6
+            dt.Columns.Add("Salvage", typeof(decimal)); // 7
+            dt.Columns.Add("Profit", typeof(decimal)); // 8
 
-            //for (int i = 0; i < system.SimulationTable.Count; i++)
-            //{
-            //    SimulationCase simulationCase = system.SimulationTable[i];
-            //    object[] obj = new object[dt.Columns.Count];
-            //    int index = 0;
-            //    obj[index++] = simulationCase.CustomerNumber;
-            //    obj[index++] = (i == 0) ? 0 : simulationCase.RandomInterArrival;
-            //    obj[index++] = simulationCase.InterArrival;
-            //    obj[index++] = simulationCase.ArrivalTime;
-            //    obj[index++] = simulationCase.RandomService;
-            //    for (int serverId = 1; serverId <= system.NumberOfServers; ++serverId)
-            //    {
-            //        if (serverId == simulationCase.AssignedServer.ID)
-            //        {
-            //            obj[index++] = simulationCase.StartTime;
-            //            obj[index++] = simulationCase.ServiceTime;
-            //            obj[index++] = simulationCase.EndTime;
-            //        }
-            //        else
-            //        {
-            //            obj[index++] = -1;
-            //            obj[index++] = -1;
-            //            obj[index++] = -1;
-            //        }
-            //    }
-            //    obj[index] = simulationCase.TimeInQueue;
-            //    dt.Rows.Add(obj);
-            //}
+            foreach(SimulationCase simulationCase in system.SimulationTable)
+            {
+                object[] obj = new object[dt.Columns.Count];
+                obj[0] = simulationCase.DayNo;
+                obj[1] = simulationCase.RandomNewsDayType;
+                obj[2] = simulationCase.NewsDayType.ToString();
+                obj[3] = simulationCase.RandomDemand;
+                obj[4] = simulationCase.Demand;
+                obj[5] = simulationCase.SalesProfit;
+                obj[6] = simulationCase.LostProfit;
+                obj[7] = simulationCase.ScrapProfit;
+                obj[8] = simulationCase.DailyNetProfit;
+                dt.Rows.Add(obj);
+            }
+            
 
             dataGridView1.DataSource = dt;
 
             dt = new DataTable();
-            //dt.Columns.Add("Average Waiting Time", typeof(decimal));
-            //dt.Columns.Add("Max Queue Length", typeof(int));
-            //dt.Columns.Add("Waiting Probability", typeof(decimal));
-            //for (int i = 0; i < system.NumberOfServers; i++)
-            //{
-            //    dt.Columns.Add("Server " + (i + 1) + ": Idle Probability", typeof(decimal));
-            //    dt.Columns.Add("Server " + (i + 1) + ": Average Service Time", typeof(decimal));
-            //    dt.Columns.Add("Server " + (i + 1) + ": Utilization", typeof(decimal));
-            //}
-            //object[] data = new object[dt.Columns.Count];
-            //int idx = 0;
-            //data[idx++] = system.PerformanceMeasures.AverageWaitingTime;
-            //data[idx++] = system.PerformanceMeasures.MaxQueueLength;
-            //data[idx++] = system.PerformanceMeasures.WaitingProbability;
-            //for (int i = 0; i < system.NumberOfServers; i++)
-            //{
-            //    data[idx++] = system.Servers[i].IdleProbability;
-            //    data[idx++] = system.Servers[i].AverageServiceTime;
-            //    data[idx++] = system.Servers[i].Utilization;
-            //}
-            //dt.Rows.Add(data);
+            dt.Columns.Add("Total Sales Profit", typeof(decimal)); // 0
+            dt.Columns.Add("Total Cost", typeof(decimal)); // 1
+            dt.Columns.Add("Total Lost Profit", typeof(decimal)); // 2
+            dt.Columns.Add("Total Scrap Profit", typeof(decimal)); // 3
+            dt.Columns.Add("Total Net Profit", typeof(decimal)); // 4
+            dt.Columns.Add("Days With More Demand", typeof(int)); // 5
+            dt.Columns.Add("Days With Unsold Papers", typeof(int)); // 6
+            object[] data = new object[dt.Columns.Count];
+            data[0] = system.PerformanceMeasures.TotalSalesProfit;
+            data[1] = system.PerformanceMeasures.TotalCost;
+            data[2] = system.PerformanceMeasures.TotalLostProfit;
+            data[3] = system.PerformanceMeasures.TotalScrapProfit;
+            data[4] = system.PerformanceMeasures.TotalNetProfit;
+            data[5] = system.PerformanceMeasures.DaysWithMoreDemand;
+            data[6] = system.PerformanceMeasures.DaysWithUnsoldPapers;
+            dt.Rows.Add(data);
             dataGridView2.DataSource = dt;
         }
 
