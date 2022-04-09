369bd37f2655aa0c9558f2595d88890c3a26ef09
aKhfagy <52210948+aKhfagy@users.noreply.github.com>
2022-04-09 22:48:39 +0200

    Added GUI similar to task 1

---
+++ NewspaperSellerSimulation/DataView.cs
@@ -0,0 +1,99 @@
+﻿using NewspaperSellerModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewspaperSellerSimulation
+{
+    public partial class DataView : Form
+    {
+        public DataView(SimulationSystem system)
+        {
+            InitializeComponent();
+            DataTable dt = new DataTable();
+            //dt.Columns.Add("Customer No.", typeof(int));
+            //dt.Columns.Add("Random Digit For Arrivals", typeof(int));
+            //dt.Columns.Add("Time Between Arrivals", typeof(int));
+            //dt.Columns.Add("Clock Time of Arrivals", typeof(int));
+            //dt.Columns.Add("Random Digit For Service", typeof(int));
+            //for (int i = 0; i < system.NumberOfServers; i++)
+            //{
+            //    dt.Columns.Add("Server " + (i + 1) + ": Time Service Begins", typeof(int));
+            //    dt.Columns.Add("Server " + (i + 1) + ": Service Time", typeof(int));
+            //    dt.Columns.Add("Server " + (i + 1) + ": Time Service Ends", typeof(int));
+            //}
+            //dt.Columns.Add("Time in Queue", typeof(int));
+
+            //for (int i = 0; i < system.SimulationTable.Count; i++)
+            //{
+            //    SimulationCase simulationCase = system.SimulationTable[i];
+            //    object[] obj = new object[dt.Columns.Count];
+            //    int index = 0;
+            //    obj[index++] = simulationCase.CustomerNumber;
+            //    obj[index++] = (i == 0) ? 0 : simulationCase.RandomInterArrival;
+            //    obj[index++] = simulationCase.InterArrival;
+            //    obj[index++] = simulationCase.ArrivalTime;
+            //    obj[index++] = simulationCase.RandomService;
+            //    for (int serverId = 1; serverId <= system.NumberOfServers; ++serverId)
+            //    {
+            //        if (serverId == simulationCase.AssignedServer.ID)
+            //        {
+            //            obj[index++] = simulationCase.StartTime;
+            //            obj[index++] = simulationCase.ServiceTime;
+            //            obj[index++] = simulationCase.EndTime;
+            //        }
+            //        else
+            //        {
+            //            obj[index++] = -1;
+            //            obj[index++] = -1;
+            //            obj[index++] = -1;
+            //        }
+            //    }
+            //    obj[index] = simulationCase.TimeInQueue;
+            //    dt.Rows.Add(obj);
+            //}
+
+            dataGridView1.DataSource = dt;
+
+            dt = new DataTable();
+            //dt.Columns.Add("Average Waiting Time", typeof(decimal));
+            //dt.Columns.Add("Max Queue Length", typeof(int));
+            //dt.Columns.Add("Waiting Probability", typeof(decimal));
+            //for (int i = 0; i < system.NumberOfServers; i++)
+            //{
+            //    dt.Columns.Add("Server " + (i + 1) + ": Idle Probability", typeof(decimal));
+            //    dt.Columns.Add("Server " + (i + 1) + ": Average Service Time", typeof(decimal));
+            //    dt.Columns.Add("Server " + (i + 1) + ": Utilization", typeof(decimal));
+            //}
+            //object[] data = new object[dt.Columns.Count];
+            //int idx = 0;
+            //data[idx++] = system.PerformanceMeasures.AverageWaitingTime;
+            //data[idx++] = system.PerformanceMeasures.MaxQueueLength;
+            //data[idx++] = system.PerformanceMeasures.WaitingProbability;
+            //for (int i = 0; i < system.NumberOfServers; i++)
+            //{
+            //    data[idx++] = system.Servers[i].IdleProbability;
+            //    data[idx++] = system.Servers[i].AverageServiceTime;
+            //    data[idx++] = system.Servers[i].Utilization;
+            //}
+            //dt.Rows.Add(data);
+            dataGridView2.DataSource = dt;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void DataView_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NewspaperSellerSimulation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewspaperSellerTesting;
+using NewspaperSellerModels;
+
+namespace NewspaperSellerSimulation
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FileNameForum());
+            if (SimulationSystem.PATH.Length > 0)
+            {
+                SimulationSystem system = new SimulationSystem();
+
+                if (SimulationSystem.PATH[SimulationSystem.PATH.Length - 5] == '1')
+                {
+                    MessageBox.Show("Test Case 1");
+                    string result = TestingManager.Test(system, Constants.FileNames.TestCase1);
+                    MessageBox.Show(result);
+                }
+                else if (SimulationSystem.PATH[SimulationSystem.PATH.Length - 5] == '2')
+                {
+                    MessageBox.Show("Test Case 2");
+                    string result = TestingManager.Test(system, Constants.FileNames.TestCase2);
+                    MessageBox.Show(result);
+                }
+                else if (SimulationSystem.PATH[SimulationSystem.PATH.Length - 5] == '3')
+                {
+                    MessageBox.Show("Test Case 3");
+                    string result = TestingManager.Test(system, Constants.FileNames.TestCase3);
+                    MessageBox.Show(result);
+                }
+                Application.Run(new DataView(system));
+            }
+        }
+    }
+}
